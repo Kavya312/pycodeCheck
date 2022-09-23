9323c4ade86c72625a9768750fa3c23a261c770f
Kavya312 <kavyanarayan312@gmail.com>
2022-09-23 11:44:39 +0530

    Create PRINT_HELLO.py

---
+++ PRINT_HELLO.py
@@ -0,0 +1,13 @@
+"""
+function for printing hello world.
+"""
+import os
+import json
+import printText
+
+def main():
+    print("Hello World!")
+    printText.ptfile()
+
+if __name__ == "__main__":
+    main()
